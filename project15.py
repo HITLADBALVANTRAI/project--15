82dc66117d5da0778f727736558de88e1c1288bf
HITLADBALVANTRAI <101315912+HITLADBALVANTRAI@users.noreply.github.com>
2022-04-03 11:37:39 +0530

    Add files via upload

---
+++ project15.py
@@ -0,0 +1,37 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor("OliveDrab")
+turtle.setpos(-50, 0)
+
+def repeated_tasks(c,s,a):
+    turtle.fillcolor(c)
+    
+    turtle.begin_fill()
+    for i in [0, 1, 2]:
+        turtle.forward(s)
+        turtle.left(a)
+    turtle.end_fill()        
+repeated_tasks("Olive",175,120)
+
+turtle.right(90)
+repeated_tasks("PeachPuff",175,90)
+    
+turtle.pendown()
+
+turtle.forward(25)
+turtle.right(90)
+turtle.begin_fill()
+
+turtle.circle(15)
+turtle.penup()
+turtle.forward(62.5)
+turtle.left(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.pendown()
+turtle
+
+
+turtle.hideturtle()
+
